61bdfa5326f7a54c9f88f176e8c37b0f79e9c117
exxhippie <48165172+exxhippie@users.noreply.github.com>
2019-04-26 04:56:35 +0000

    Add files via upload

---
+++ dynamic-test.js
@@ -0,0 +1,20 @@
+//dynamic test
+var home = require("os").homedir();
+var logpath = home + '/Documents/routes/routes.js';
+const routes = require(logpath);
+const express = require('express');
+const port = 3002;
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true,
+}));
+
+routes(app);
+
+const server = app.listen(port, (error) => {
+	if (error) return console.log(`error: ${error}`);
+	console.log(`server listening on port ${server.address().port}`);
+});
--- routes.js
+++ routes.js
@@ -1,17 +1,39 @@
+//dynamic route
 const router = app => {
-	app.get('/users', (request, response) => {
-		pool.query('SELECT * FROM users', (error, result) => {
+	/*app.get('/users/:id', (request, response) => {
+		const id = request.params.id;
+		pool.query('SELECT * FROM users WHERE id = 4', (error, result) => {
 			if (error) throw error;
 			response.send(result);
 		});
+	});*/
+	app.post('/users', (request, response) => {
+		pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+			if (error) throw error;
+			response.status(201).send(`user added with id: ${result.insertid}`);
+		});
 	});
-}
+	/*app.put('/users/:id', (request, reponse) => {
+		const id = request.params.id;
+		pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
+			if (error) throw error;
+			response.send('user updated successfully');
+		});
+	});*/
+	/*app.delete('/users/:id', (request, response) => {
+		const id = request.params.id;
+		pool.query('DELETE FROM users WHERE id = ?', id, (err, result) => {
+			if (error) throw error;
+			response.send('user deleted');
+		});
+	});*/
+};
 module.exports = router;
 
 var home = require("os").homedir();
 var logpath = home + '/Documents/node.js/config.js';
 const pool = require(logpath);
+
 const users = [{
 	pool
-},
-];
+}];
