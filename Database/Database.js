2f28647fd76b9a3fe34120e7482ca4ba0001eb56
Kwame-Addo <48158461+Kwame-Addo@users.noreply.github.com>
2019-06-08 08:07:42 +0000

    Update Database.js

--- Database/Database.js
+++ Database/Database.js
@@ -24,51 +24,51 @@ var db = mysql.createConnection({
     multipleStatements: true
 });
 //creating admin table
-var sql = CREATE TABLE admin (
+var sql = "CREATE TABLE admin (
     email VARCHAR(45) PRIMARY KEY, 
     password VARCHAR(125)
-);
+)";
 //creating signup table
-var sql = CREATE TABLE signUp (
+var sql = "CREATE TABLE signUp (
     name VARCHAR(125),
     institution_name VARCHAR(125),
     email VARCHAR(45) PRIMARY KEY, 
     password VARCHAR(125), 
     phone_number INT
-    );
+    )";
 //creating tables for school calendar 
-var sql = CREATE TABLE school_calendar(
+var sql = "CREATE TABLE school_calendar(
     event_name VARCHAR(125),
     description VARCHAR(256),
     select_date DATE, 
     id INT AUTO_INCREMENT PRIMARY KEY)
-    );
+    )";
 //creating tables for class timetable 
-var sql = CREATE TABLE period(
+var sql = "CREATE TABLE period(
     period VARCHAR(125),
     id AUTO_INCREMENT PRIMARY KEY
-    );
-var sql = CREATE TABLE day_of_week(
+    )";
+var sql = "CREATE TABLE day_of_week(
     id INT AUTO_INCREMENT PRIMARY KEY,
     day VARCHAR(125)
-    );
-var sql = CREATE TABLE name_of_class(
+    )";
+var sql = "CREATE TABLE name_of_class(
     id INT AUTO_INCREMENT PRIMARY KEY,
     class_name VARCHAR(45)
-    );
-var sql = CREATE TABLE class_timetable(
+    )";
+var sql = "CREATE TABLE class_timetable(
     id INT PRIMARY KEY,
     class_name VARCHAR(125), 
     period_id INT, day_id INT
-    );
+    )";
 //creating tables for billing and payments
-var sql = CREATE TABLE outstanding(
+var sql = "CREATE TABLE outstanding(
     student_names VARCHAR(125), 
     amount_owing INT, 
     id AUTO INCREMENT PRIMARY KEY
-    );
+    )";
 //creating tables for enrollment
-var sql = CREATE TABLE add_student(
+var sql = "CREATE TABLE add_student(
     add_class VARCHAR(125), 
     student_name VARCHAR(125), 
     student_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -77,36 +77,36 @@ var sql = CREATE TABLE add_student(
     mother_num INT,father_name VARCHAR(125), 
     father_email VARCHAR(125), 
      father_num INT
-     );
+     )";
 //creating tables for classrooms
-var sql = CREATE TABLE classrooms(
+var sql = "CREATE TABLE classrooms(
     add_classroom VARCHAR(125), 
     classrooms VARCHAR(125) PRIMARY KEY,
     classes_created_so_far INT, 
     programmes VARCHAR(125)
-     );
+     )";
 //creating tables for students
-var sql = CREATE TABLE students(
+var sql = "CREATE TABLE students(
     student_id INT AUTO_INCREMENT PRIMARY KEY,
     student_name VARCHAR(45),
     student_class VARCHAR(45)
-    );
+    )";
 //creating tables for teachers
-var sql = CREATE TABLE teachers(
+var sql = "CREATE TABLE teachers(
     teacher_id INT PRIMARY KEY,
     teacher-name VARCHAR(125)
-    );
+    )";
 //creating tables for daily reports
- var sql = CREATE TABLE daily_report(
+ var sql = "CREATE TABLE daily_report(
      date DATE,
      report_name VARCHAR(125),
      report BLOB,
-    );
+    )";
  //creating tables for statistics
- var sql = CREATE TABLE statistics(
+ var sql = "CREATE TABLE statistics(
      mesages_sent INT,
      events_sent INT, 
      students_joined INT, 
      teachers_joined INT
-     );
+     )";
      
